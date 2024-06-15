fef89d4f25c62068c4a2354fafac290531cee811
knightbane186 <denverlopes93@gmail.com>
2024-06-15 18:50:37 +1000

    Quick update

--- web_App/script.js
+++ web_App/script.js
@@ -10,3 +10,12 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+//learning how to use the geo location API
+if(navigator.geolocation)
+navigator.geolocation.getCurrentPosition(function(position){
+const {latitude} = position.coords;
+const{longitude} = position.coords;
+console.log(latitude,longitude);
+},function(){
+    alert('Could not get your position')
+});
