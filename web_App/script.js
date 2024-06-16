1c77aab6043665d4c276b1400faf3d580423d55a
knightbane186 <denverlopes93@gmail.com>
2024-06-17 00:40:58 +1000

    Quick update

--- web_App/script.js
+++ web_App/script.js
@@ -13,30 +13,49 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 let map, mapEvent;
 
-// Learning how to use the geo location API
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
+class App {
+    #map;
+    #mapEvent;
+    constructor() {
+        this._getPosition();
+        form.addEventListener('submit', this._newWorkout.bind(this));
+        inputType.addEventListener('change', this._toggleElevationField);
+    }
+
+    _getPosition() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function () {
+                alert('Could not get your position');
+            });
+        }
+    }
+
+    _loadMap(position) {
         const { latitude, longitude } = position.coords;
         console.log(`https://www.google.com.au/maps/@${latitude},${longitude}`);
         const coords = [latitude, longitude];
-        map = L.map('map').setView(coords, 13);
+        this.#map = L.map('map').setView(coords, 13);
 
         L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+        }).addTo(this.#map);
 
         // Handling clicks on map
-        map.on('click', function (mapE) {
-            mapEvent = mapE;
-            form.classList.remove('hidden');
-            inputDistance.focus();
-        });
+        this.#map.on('click', this._showForm.bind(this));
+    }
 
-    }, function () {
-        alert('Could not get your position');
-    });
+    _showForm(mapE) {
+        this.#mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+    }
 
-    form.addEventListener('submit', function (e) {
+    _toggleElevationField() {
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    }
+
+    _newWorkout(e) {
         e.preventDefault();
 
         // Clear input fields
@@ -56,10 +75,7 @@ if (navigator.geolocation) {
             })
             .setContent('Workout'))
             .openPopup();
-    });
-
-    inputType.addEventListener('change', function () {
-        inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-        inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-    });
+    }
 }
+
+const app = new App();
